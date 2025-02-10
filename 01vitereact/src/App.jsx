f24cdd5e5f511c16c681abb9b541d656d7d66c6d
Raja Makhdoom <rajamuhammadmakhdoomhussain@gmail.com>
2025-02-09 15:47:41 -1100

    interview

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>chai aur react | makhdoom</h1>;
+}
+
+export default App;
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit Google",
+// };
+
+// const anotherElement = (
+//   <a href="https://google.com" target="_blank">
+//     Visit Google
+//   </a>
+// );
+
+const anotherUser = "chai aur react";
+const reactElement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  "click me to visit google ",
+  anotherUser
+);
+
+createRoot(document.getElementById("root")).render(reactElement);
